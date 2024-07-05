2420516290f11ee0e7b18709d5484b69399b71b4
Void-Individual <otayomitv@gmail.com>
2024-07-05 01:30:27 +0100

    Used mypy to check

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 """Type annotated module"""
 
-from typing import Sequence, Any, Union, Tuple
+from typing import Sequence, Any, Union, Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Function to increase the size of an array by a factor"""
+
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
