fa45b318e295adc9b0f34ca64b7d0b1e2550e091
Void-Individual <otayomitv@gmail.com>
2024-07-29 00:13:52 +0100

    God Please

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 from parameterized import parameterized
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -50,6 +51,32 @@ class TestGetJson(unittest.TestCase):
         # Test that the get method was called only once per input
         mock.assert_called_once()
         patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """Test class to affirm the memoize decorator"""
+
+    def test_memoize(self):
+        """Method to match the expected result of the memoize decorator"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_class = TestClass()
+        with patch.object(test_class, 'a_method',
+                          return_value=42) as mock_method:
+            result1 = test_class.a_property
+            result2 = test_class.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
