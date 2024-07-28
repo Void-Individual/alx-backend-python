b6af969cace592f0c618cea6488152febc40b6e4
Void-Individual <otayomitv@gmail.com>
2024-07-29 00:48:45 +0100

    Reverted buk changes

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,63 +1,61 @@
 #!/usr/bin/env python3
-'''Module to test utils file
-'''
-from parameterized import parameterized
+"""Test module for the utils file"""
+
 import unittest
-from utils import (access_nested_map, get_json, memoize)
 from unittest.mock import patch
+from parameterized import parameterized
+from utils import (access_nested_map, get_json, memoize)
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''class for testing access_nestd_map function
-    '''
+    """To inherit from a unittest method and check it"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Test that the method returns what it is supposed to
-        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), 'a'),
-        ({"a": 1}, ("a", "b"), 'b')
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        """ Test that a KeyError is raised for the respective inputs """
-        with self.assertRaises(KeyError) as e:
+    def test_access_nested_map_exception(self, nested_map, path, error):
+        with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+        self.assertEqual(repr(context.exception), f"KeyError('{error}')")
 
 
 class TestGetJson(unittest.TestCase):
-    """ Class for Testing Get Json """
+    """Class to check the get_json function"""
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    def test_get_json(self, test_url, test_payload):
-        """ Test for the utils.get_json function to check
-        that it returns the expected result."""
+    def test_get_json(self, url, test_payload):
+        """Test to confirm that the utils.get_json function returns
+        the expected result"""
+
         config = {'return_value.json.return_value': test_payload}
         patcher = patch('requests.get', **config)
         mock = patcher.start()
-        self.assertEqual(get_json(test_url), test_payload)
+        result = get_json(url)
+
+        # Test thata the get utput is equal to payload
+        self.assertEqual(result, test_payload)
+        # Test that the get method was called only once per input
         mock.assert_called_once()
         patcher.stop()
 
 
 class TestMemoize(unittest.TestCase):
-    """ test class to tes utils.memoize"""
+    """Test class to affirm the memoize decorator"""
 
     def test_memoize(self):
-        """ Tests the function when calling a_property twice,
-        the correct result is returned but a_method is only
-        called once using assert_called_once
-        """
+        """Method to match the expected result of the memoize decorator"""
 
         class TestClass:
             """ Test Class for wrapping with memoize """
@@ -69,8 +67,15 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method') as mock:
+        with patch.object(TestClass, 'a_method') as mock_method:
+            # mock_method.return_value = 42
             test_class = TestClass()
             test_class.a_property()
             test_class.a_property()
-            mock.assert_called_once()
+            # self.assertEqual(result1, 42)
+            # self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
