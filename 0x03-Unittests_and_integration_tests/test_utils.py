a585f59577831f21ffe671347f1f3272909fba1a
Void-Individual <otayomitv@gmail.com>
2024-07-29 00:35:08 +0100

    called the function instead of listing it

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -71,8 +71,8 @@ class TestMemoize(unittest.TestCase):
         with patch.object(TestClass, 'a_method') as mock_method:
             # mock_method.return_value = 42
             test_class = TestClass()
-            result1 = test_class.a_property
-            result2 = test_class.a_property
+            result1 = test_class.a_property()
+            result2 = test_class.a_property()
             # self.assertEqual(result1, 42)
             # self.assertEqual(result2, 42)
             mock_method.assert_called_once()
