99eab6be4046cf2e8e3a6f5c91b1153d48b20649
Void-Individual <otayomitv@gmail.com>
2024-07-28 23:15:00 +0100

    assertraises and mock.patch

--- 0x03-Unittests_and_integration_tests/main.py
+++ 0x03-Unittests_and_integration_tests/main.py
@@ -2,5 +2,8 @@
 """Test module for the utils file"""
 
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
-print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a", "c")))
+print(access_nested_map(nested_map={"a": {"b": 2}}, path=("a", "b")))
+val = get_json("http://holberton.io")
+print(val)
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 import unittest
 from parameterized import parameterized
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,6 +17,35 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class to check the get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @unittest.mock.patch('requests.get')
+    def test_get_json(self, url, test_payload, mock_get):
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(url)
+
+        # Test thata the get utput is equal to payload
+        self.assertEqual(result, test_payload)
+        # Test that the get method was called only once per input
+        mock_get.assert_called_once_with(url)
 
 
 if __name__ == '__main__':
