30e89cfe6a1431c003ad2164a3d1438b1dc29af8
Void-Individual <otayomitv@gmail.com>
2024-07-29 22:16:36 +0100

    Asserting equal

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -70,12 +70,12 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method') as mock_method:
-            # mock_method.return_value = 42
+            mock_method.return_value = 42
             test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            # self.assertEqual(result1, 42)
-            # self.assertEqual(result2, 42)
+            result1 = test_class.a_property
+            result2 = test_class.a_property
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
             mock_method.assert_called_once()
 
 
